ef942b009cf581b3f2b06f8edf56c17ed72f17c6
malverdo <malverdo11@mail.ru>
2022-01-22 15:26:00 +0300

    pgsql connect

---
+++ PhpUnit/tests/DatabaseTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests;
+
+
+use PDO;
+use PHPUnit\Framework\TestCase;
+
+
+class DatabaseTest extends TestCase
+{
+    protected static $dbh;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$dbh = new PDO('pgsql:host=172.17.0.1;port=54321;dbname=test','postgres','root');
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$dbh = null;
+    }
+
+    public function testOne()
+    {
+        $this->assertSame(1,1);
+    }
+}
