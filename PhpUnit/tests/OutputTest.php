e058ce6740de7de737405da626999b218235195f
malverdo <malverdo11@mail.ru>
2022-01-22 14:20:06 +0300

    Output

---
+++ PhpUnit/tests/OutputTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class OutputTest extends TestCase
+{
+    public function testExpectFooActualFoo()
+    {
+        $this->expectOutputString('foo');
+        print 'foo';
+    }
+
+    public function testExpectBarActualBaz()
+    {
+        $this->setOutputString();
+        print 'bar';
+        $getBar = $this->getActualOutput();
+    }
+    private function setOutputString()
+    {
+        $this->expectOutputString('bar');
+
+    }
+}
